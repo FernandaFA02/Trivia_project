f6245d6687e828ff4f2e7e3a324d94cf39adb5c0
FernandaFA02 <fajardo.al9402@mail.com>
2021-09-23 23:26:27 -0500

    Subiendo trivia

--- trivia.js
+++ trivia.js
@@ -5,10 +5,10 @@ Proyecto 1
 
 //Bienvenida
 var app = "Trivia de México" 
-var userName = prompt("Hola, Bienvenid@, porfavor ingresa tu nombre")
+var userName = prompt("Hola, Bienvenid@ a la trivia sobre México, porfavor ingresa tu nombre")
 console.log(userName)// muestra el nombre el usuario
 
-document.getElementById("title").innerHTML ="Proyecto de " + app
+document.getElementById("title").innerHTML ="Proyecto " + app
 
 document.getElementById("user_name").innerHTML ="Bienvenid@ " + userName
 
@@ -16,172 +16,135 @@ var curiosity = confirm("Gustas realizar esta encuesta?")
 var correctCuriosity = true
 
 if (curiosity == correctCuriosity){
-    alert("Gracias")
+    alert("Gracias, Comencemos!. Por favor escribe el número correspondiente a tu respuesta.")
 
 //Comienzan preguntas y respuestas
 
 //Pregunta 1
-var question1 = prompt("El santurio de las mariposas Monarca se encuentra en?")
+var question1 = prompt("El santuario de las mariposas Monarca se encuentra en? \n 1.Sinaloa \n 2.Michoacán \n 3.Puebla")
 console.log(question1)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer1").innerHTML = ("Tu respuesta 1 " + question1)
-//Respuesta correcta 1
-var correctAnswer1 = "Michoacan"
-//Acceder al html desde JavaScript
-document.getElementById("correct_answer1").innerHTML = "La respuesta correcta era " + correctAnswer1
 //Alerta de respuestas
-if (question1 == correctAnswer1) {
-    alert(":)")
+if (question1 == 2) {
+    alert("Correcto! :)")
 } else {
     alert(":( ")
 } 
+//Accede al html desde JavaScript
+document.getElementById("answer1").innerHTML = question1
 
 //Pregunta 2
-var question2 = prompt("¿En que estado se encuentra la Riviera Maya?")
+var question2 = prompt("¿En que estado se encuentra la Riviera Maya? \n 1.Yucatán \n 2.Campeche \n 3.Quintana Roo")
 console.log(question2)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer2").innerHTML = "Tu respuesta 2 " + question2
-//Respuesta correcta 2
-var correctAnswer2 = "Quintana Roo"
-//Acceder al html desde JavaScript
-document.getElementById("correct_answer2").innerHTML = "La respuesta correcta era " + correctAnswer2
 //Alerta de respuestas
-if (question2 == correctAnswer2) {
-    alert(":)")
+if (question2 == 3) {
+    alert("Excelente!")
 } else {
-    alert (":(")
+    alert (":/")
 }
+//Accede al html desde JavaScript
+document.getElementById("answer2").innerHTML = question2
 
 //Pregunta 3
-var question3 = prompt("Nombra los 3 principales estados que abarca la Huasteca")
+var question3 = prompt("Las cascadas Hierve el agua, se encuentran en? \n 1.Chiapas \n 2.San Luis Potosi \n 3.Oaxaca")
 console.log(question3)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer3").innerHTML = "Tu respuesta 3 " + question3
-//Respuesta correcta 3
-var correctAnswer3 = ["San Luis Potosi", "Veracruz", "Hidalgo"]
-//Acceder al html desde JavaScript
-document.getElementById("correct_answer3").innerHTML = "La respuesta correcta era " + correctAnswer3
 //Alerta de respuestas
-if (question3 == correctAnswer3) {
-    alert(":)")
+if (question3 == 3) {
+    alert("Muy bien!!")
 } else {
-    alert(":(")
+    alert(":/")
 }
+//Accede al html desde JavaScript
+document.getElementById("answer3").innerHTML = question3
 
 //Pregunta 4
-var question4 = prompt("¿Cuál es la ciudad que cuenta con más museos en el país?")
+var question4 = prompt("¿Cuál es la ciudad que cuenta con más museos en el país? \n 1.CDMX \n 2.Guadalajara \n 3.Monterrey")
 console.log(question4)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer4").innerHTML = "Tu respuesta 4 " + question4
-//Respuesta correcta 4
-var correctAnswer4 = "Ciudad de México"
-//Acceder al html desde JavaScript
-document.getElementById("received_answer4").innerHTML = "La respuesta correcta era " + correctAnswer4
 //Alerta de respuestas
-if (question4 == correctAnswer4) {
-    alert(":)")
+if (question4 == 1) {
+    alert("Asi se hace!")
 } else {
-    alert(":(")
+    alert(":/")
 }
+//Accede al html desde JavaScript
+document.getElementById("answer4").innerHTML = question4
 
 //Pregunta 5
-var question5 = prompt("¿Que lugar es conocido como 'El lugar donde habitan los brujos'?")
+var question5 = prompt("¿Que lugar es conocido como 'El lugar donde habitan los brujos'? \n 1.Catemaco \n 2.Coatepec \n 3.Calcahualco")
 console.log(question5)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer5").innerHTML = "Tu respuesta 5 " + question5
-//Respuesta correcta 5
-var correctAnswer5 = "Catemaco"
-//Acceder al html desde JavaScript
-document.getElementById("correct_answer5").innerHTML = "La respuesta correcta era " + correctAnswer5
 //Alerta de respuestas
-if (question5 == correctAnswer5) {
-    alert(":)")
+if (question5 == 1) {
+    alert("Perfecto!!")
 } else {
-    alert(":(")
+    alert(":/")
 }
+//Accede al html desde JavaScript
+document.getElementById("answer5").innerHTML = question5
 
 //Pregunta 6
-var question6 = prompt("¿En que estado se encuentra hubicado Tequila?")
+var question6 = prompt("¿En que estado se encuentra hubicado Tequila? \n 1.Jalisco \n 2.San Luis Potosi \n 3.Nayarit")
 console.log(question6)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer6").innerHTML = "Tu respuesta 6 " + question6
-//Respuesta correcta 6
-var correctAnswer6 = "Jalisco"
-//Acceder al html desde JavaScript
-document.getElementById("correct_answer6").innerHTML = "La respuesta correcta era " + correctAnswer6
 //Alerta de respuestas
-if (question6 == correctAnswer6) {
-    alert(":)")
+if (question6 == 1) {
+    alert("Sigue asi!!!")
 } else {
     alert(":(")
 }
+//Accede al html desde JavaScript
+document.getElementById("answer6").innerHTML = question6
 
 //Pregunta 7
-var question7 = prompt("Principal grupo étnico al que pertenecen los voladores de Papantla...?")
+var question7 = prompt("Principal grupo étnico al que pertenecen los voladores de Papantla...? \n 1.Zapoteca \n 2.Totonaca \n 3.Mazateco")
 console.log(question7)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer7").innerHTML = "Tu respuesta 7 " + question7
-//Respuesta correcta 7
-var correctAnswer7 = "Totonaco"
-//Acceder al html desde JavaScript
-document.getElementById("correct_answer7").innerHTML = "La respuesta correcta era " + correctAnswer7
 //Alerta de respuestas
-if (question7 == correctAnswer7) {
-    alert(":)")
+if (question7 == 2) {
+    alert("Eso es! :)")
 } else {
     alert(":(")
 }
+//Accede al html desde JavaScript
+document.getElementById("answer7").innerHTML = question7
 
 //Pregunta 8
-var question8 = prompt("¿Como se llama el sitio turistico más famoso en Los Cabos?")
+var question8 = prompt("¿Como se llama el sitio turistico más famoso en Los Cabos? \n 1.Playa el Tule \n 2.Parque Nacional Cabo Pulmo \n 3.El arco")
 console.log(question8)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer8").innerHTML =  "Tu respuesta 8 " + question8
-//Respuesta correcta 8
-var correctAnswer8 = "El arco"
-//Acceder al html desde JavaScript
-document.getElementById("correct_answer8").innerHTML = "La respuesta correcta era " + correctAnswer8
 //Alerta de respuestas
-if (question8 == correctAnswer8) {
-    alert(":)")
+if (question8 == 3) {
+    alert("Asi se hace!")
 } else {
-    alert(":(")
+    alert("Tú puedes, intentalo :(")
 }
+//Accede al hmtl desde JavaScript
+document.getElementById("answer8").innerHTML = question8
 
 //Pregunta 9
-var question9 = prompt("¿Comó se llama el templo (piramide) ubicado en Chichén Itzá?")
+var question9 = prompt("¿Comó se llama el templo (pirámide) ubicado en Chichén Itzá? \n 1.Pirámide de los nichos \n 2.Kukulcán \n 3.Calakmul")
 console.log(question9)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer9").innerHTML = "Tu respuesta 9 " + question9
-//Respuesta correcta 9
-var correctAnswer9 = "Kukulcán"
-//Acceder al html desde JavaScript
-document.getElementById("correct_answer9").innerHTML = "La respuesta correcta era " + correctAnswer9
 //Alerta de respuestas
-if (question9 == correctAnswer9) {
-    alert(":)")
+if (question9 == 2) {
+    alert("Super!!")
 } else {
-    alert(":(")
+    alert("Ánimo, solo falta una :/")
 }
+//Accede al hmtl desde JavaScript
+document.getElementById("answer9").innerHTML = question9
 
 //Pregunta 10
-var question10 = prompt("Nombra los 3 tipos de civilizaciones que se pueden observar en los edificios de la Plaza de las 3 Culturas")
+var question10 = prompt("¿En donde se encuentra ubicado el Pueblo Mágico de San Pedro Cholula? \n 1.Puebla \n 2.Queretaro \n 3.Hidalgo")
 console.log(question10)
-//Acceder al html desde JavaScript
-document.getElementById("received_answer10").innerHTML = "Tu respuesta 10 " + question10
-//Respuesta correcta 10
-var correctAnswer10 = ["precolombina","colonial","moderna"]
-//Acceder al html desde JavaScript
-document.getElementById("correct_answer10").innerHTML = "La respuesta correcta " + correctAnswer10
 //Alerta de respuestas
-if (question10 == correctAnswer10) {
-    alert(":)")
+if (question10 == 1) {
+    alert("Harry, eres un mago!!!")
 } else{
-    alert(":(")
+    alert("Al menos lo intentaste ")
 }
+//Acceder al html desde JavaScript
+document.getElementById("answer10").innerHTML = question10
 
 }else {
-    alert("Ok, Bye!")
+    alert("Antes de que te vayas, te daré unos datos curiosos...")
+    alert("¿Sabías que la televisión en color fue inventada en México? También son inventos mexicanos la tinta indeleble y la píldora anticonceptiva.")
+    alert("El cráter de Chicxulub al norte de la península de Yucatán es el lugar donde, según algunas teorías, cayó el meteorito que acabó con los dinosaurios.")
+    alert("Ok eso es todo por el momento, que tengas lindo día Bye!")
 }
 
 
